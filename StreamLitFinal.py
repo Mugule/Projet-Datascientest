96f00947f2eed14bfdf8282ee0a74d6036ef7d60
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-03-15 10:05:30 +0100

    Update StreamLitFinal.py

--- StreamLitFinal.py
+++ StreamLitFinal.py
@@ -566,11 +566,11 @@ if selectedMenu == "Methodologie":
 
         # Création d'une liste d'images
         images = [
-            image.open("medias/NaiveBayes_ConfusionMatrix_black.png")
+            image.open("medias/NaiveBayes_ConfusionMatrix_black.png"),
             image.resize((300, 300)),
-            image.open("medias/RandomForest_ConfusionMatrix_black.png")
+            image.open("medias/RandomForest_ConfusionMatrix_black.png"),
             image.resize((300, 300)),
-            image.open("medias/SVM_ConfusionMatrix_black.png")
+            image.open("medias/SVM_ConfusionMatrix_black.png"),
             image.resize((300, 300))
         ]
 
